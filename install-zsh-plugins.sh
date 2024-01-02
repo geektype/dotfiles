c1b77c8e544679b61c48d4ae2597e31cf2b4d951
Abdullah Ahmed <12223088+geektype@users.noreply.github.com>
2024-01-02 20:57:03 +0000

    Install TPM

--- install-zsh-plugins.sh
+++ install-zsh-plugins.sh
@@ -1,3 +1,9 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
+
+if [ ! -d "$HOME/.config/tmux/plugins" ]; then
+    mkdir -v $HOME/.config/tmux/plugins
+fi
+git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
+
